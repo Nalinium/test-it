48c65aef89a1904bf84e006bb30a4f138a742c60
Nalinium <lemairenathanael@gmail.com>
2026-01-23 17:19:17 +0100

    Test intégration 1

--- src/test/java/com/example/testit/service/TaskServiceTestIT.java
+++ src/test/java/com/example/testit/service/TaskServiceTestIT.java
@@ -5,6 +5,7 @@ import com.example.testit.model.Task;
 import com.example.testit.model.User;
 import com.example.testit.repository.TaskRepository;
 import com.example.testit.repository.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,17 +19,28 @@ public class TaskServiceTestIT {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    TaskService taskService;
+
     @Test
     public void test1(){
         Task task = new Task();
         User user = new User();
 
+        user.setId(1L);
+        user.setUsername("User");
+
         task.setId(1L);
         task.setTitle("Task1");
         task.setDescription("Description Task1");
         task.setAssignedUser(user);
-        task.setStatus(Status.EN_COURS);
+        task.setStatus(Status.OUVERT);
 
         taskRepository.save(task);
+        userRepository.save(user);
+
+        taskService.startTask(task.getId(), user.getId());
+
+        Assertions.assertThat(task.getStatus()).isEqualTo(Status.EN_COURS);
     }
 }
